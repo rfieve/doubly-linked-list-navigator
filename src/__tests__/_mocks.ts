32dfe5ad4dd73f94a7ef9871fc04ac95ac2a59a0
rfieve <48585798+rfieve@users.noreply.github.com>
2023-10-09 21:37:46 +0200

    style(lint): const declarations alignments

--- src/__tests__/_mocks.ts
+++ src/__tests__/_mocks.ts
@@ -4,7 +4,6 @@ export function compare(a: number, b: number) {
     return a - b;
 }
 
-export const mockedArray = [2, 5, 10, 13, 32, 50, 89];
-
-export const mockedDLLNavigator = new DoublyLinkedListNavigator(mockedArray, compare);
-export const mockedEmptyDLLNavigator = new DoublyLinkedListNavigator();
+export const mockedArray = [2, 5, 10, 13, 32, 50, 89],
+             mockedDLLNavigator = new DoublyLinkedListNavigator(mockedArray, compare),
+             mockedEmptyDLLNavigator = new DoublyLinkedListNavigator();
